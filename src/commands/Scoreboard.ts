91d4976475bd58818d6b34053a897741e1042bfa
Sean Frankruyter <s.frankruyter@gmail.com>
2022-03-24 23:06:41 -0400

    fix the scoring algorithm

--- src/commands/Scoreboard.ts
+++ src/commands/Scoreboard.ts
@@ -4,41 +4,52 @@ import { Database, OPEN_READONLY } from 'sqlite3';
 
 const db = new Database('./src/db.sqlite', OPEN_READONLY);
 
-interface reportRow {
-  username: string;
-  wordles: string[];
-}
-
 export const Scoreboard: Command = {
   name: 'scoreboard',
   description: 'Returns a scoreboard',
   type: 'CHAT_INPUT',
   run: async (client: Client, interaction: BaseCommandInteraction) => {
-    let content = `Summed Scores from last 10 Wordles:\nRank | User | Score`;
+    let content = 'Summed Scores from last 7 Wordles:```';
 
-    db.all(
-      `
-    SELECT username, wordleId, SUM(score) AS score 
-    FROM scores s 
-    INNER JOIN usernames u ON s.userId = u.userId
-    WHERE wordleId BETWEEN (SELECT MAX(wordleId) FROM scores) - 10 AND (SELECT MAX(wordleId) FROM scores) AND score > 0
-    GROUP BY u.userId
-    ORDER BY score ASC
-    `,
-      (err, rows) => {
-        if (err) {
-          console.log(err);
+    db.serialize(() => {
+      db.all(
+        `SELECT username, wordleId, SUM(
+            CASE score
+              WHEN 1 THEN 6
+              WHEN 2 THEN 5
+              WHEN 3 THEN 4
+              WHEN 4 THEN 3
+              WHEN 5 THEN 2
+              WHEN 6 THEN 1
+          END) AS sumScore
+      FROM scores s 
+      INNER JOIN usernames u 
+        ON s.userId = u.userId
+      WHERE wordleId BETWEEN (SELECT MAX(wordleId) FROM scores) - 7 
+        AND (SELECT MAX(wordleId) FROM scores) 
+        AND score > 0
+      GROUP BY u.userId
+      ORDER BY sumScore DESC`,
+        (err, rows) => {
+          if (err) {
+            console.error(err);
+          }
+
+          rows.forEach((row, i) => {
+            content += `\n| ${i + 1} | ${row.username} - ${row.sumScore}`;
+            if (i === 0) {
+              content += ' 🏆';
+            }
+          });
+
+          content += '```';
+
+          interaction.followUp({
+            ephemeral: false,
+            content,
+          });
         }
-
-        rows.forEach((row, i) => {
-          content += `\n${i + 1} | ${row.username} | ${row.score}`;
-        });
-
-        interaction.followUp({
-          ephemeral: false,
-          content,
-        });
-      }
-    );
+      );
+    });
   },
 };
